b3773a7c6e8fbcfb435d08e36930aecc0b4b256d
Dino Chan <dino.chan@live.com>
2018-02-27 15:33:57 +0800

    add project.

---
+++ BusyIndicatorSample/BusyIndicatorSample/Views/ImageGalleryDetailPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using BusyIndicatorSample.Helpers;
+using BusyIndicatorSample.Models;
+using BusyIndicatorSample.Services;
+
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace BusyIndicatorSample.Views
+{
+    public sealed partial class ImageGalleryDetailPage : Page, INotifyPropertyChanged
+    {
+        private DispatcherTimer _timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(500) };
+        private object _selectedImage;
+        private ObservableCollection<SampleImage> _source;
+
+        public object SelectedImage
+        {
+            get => _selectedImage;
+            set
+            {
+                Set(ref _selectedImage, value);
+                ApplicationData.Current.LocalSettings.SaveString(ImageGalleryPage.ImageGallerySelectedIdKey, ((SampleImage)SelectedImage).ID);
+            }
+        }
+
+        public ObservableCollection<SampleImage> Source
+        {
+            get => _source;
+            set => Set(ref _source, value);
+        }
+
+        public ImageGalleryDetailPage()
+        {
+            // TODO WTS: Replace this with your actual data
+            Source = SampleDataService.GetGallerySampleData();
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var sampleImage = e.Parameter as SampleImage;
+            SelectedImage = Source.FirstOrDefault(i => i.ID == sampleImage.ID);
+            var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation(ImageGalleryPage.ImageGalleryAnimationOpen);
+            animation?.TryStart(previewImage);
+            showFlipView.Begin();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                previewImage.Visibility = Visibility.Visible;
+                ConnectedAnimationService.GetForCurrentView()?.PrepareToAnimate(ImageGalleryPage.ImageGalleryAnimationClose, previewImage);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ BusyIndicatorSample/BusyIndicatorSample/Views/ImageGalleryPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using BusyIndicatorSample.Helpers;
+using BusyIndicatorSample.Models;
+using BusyIndicatorSample.Services;
+
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace BusyIndicatorSample.Views
+{
+    public sealed partial class ImageGalleryPage : Page, INotifyPropertyChanged
+    {
+        public const string ImageGallerySelectedIdKey = "ImageGallerySelectedIdKey";
+        public const string ImageGalleryAnimationOpen = "ImageGallery_AnimationOpen";
+        public const string ImageGalleryAnimationClose = "ImageGallery_AnimationClose";
+
+        private ObservableCollection<SampleImage> _source;
+
+        public ObservableCollection<SampleImage> Source
+        {
+            get => _source;
+            set => Set(ref _source, value);
+        }
+
+        public ImageGalleryPage()
+        {
+            // TODO WTS: Replace this with your actual data
+            Source = SampleDataService.GetGallerySampleData();
+            InitializeComponent();
+        }
+
+        private void ImagesGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selected = e.ClickedItem as SampleImage;
+            ImagesGridView.PrepareConnectedAnimation(ImageGalleryAnimationOpen, selected, "galleryImage");
+            NavigationService.Navigate<ImageGalleryDetailPage>(e.ClickedItem);
+        }
+
+        private async void ImagesGridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var selectedImageId = await ApplicationData.Current.LocalSettings.ReadAsync<string>(ImageGallerySelectedIdKey);
+            if (!string.IsNullOrEmpty(selectedImageId))
+            {
+                var animation = ConnectedAnimationService.GetForCurrentView().GetAnimation(ImageGalleryAnimationClose);
+                if (animation != null)
+                {
+                    var item = ImagesGridView.Items.FirstOrDefault(i => ((SampleImage)i).ID == selectedImageId);
+                    ImagesGridView.ScrollIntoView(item);
+                    await ImagesGridView.TryStartConnectedAnimationAsync(animation, item, "galleryImage");
+                }
+
+                ApplicationData.Current.LocalSettings.SaveString(ImageGallerySelectedIdKey, string.Empty);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
